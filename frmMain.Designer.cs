1d21d78f872a170f3741def456dd7bca1203b624
marimo <33587511+vrMarimo@users.noreply.github.com>
2018-12-16 05:18:36 +0900

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Monty_Hall_problem
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txtHit = new System.Windows.Forms.TextBox();
+            this.txtMiss = new System.Windows.Forms.TextBox();
+            this.txtMissRemove = new System.Windows.Forms.TextBox();
+            this.txtHitRemove = new System.Windows.Forms.TextBox();
+            this.txtRunCnt = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.gb2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.gb3 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chcSelect = new System.Windows.Forms.CheckBox();
+            this.gb1.SuspendLayout();
+            this.gb2.SuspendLayout();
+            this.gb3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHit
+            // 
+            this.txtHit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtHit.Location = new System.Drawing.Point(6, 36);
+            this.txtHit.MaxLength = 5;
+            this.txtHit.Name = "txtHit";
+            this.txtHit.Size = new System.Drawing.Size(100, 19);
+            this.txtHit.TabIndex = 0;
+            this.txtHit.Text = "1";
+            this.txtHit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtHit.WordWrap = false;
+            this.txtHit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // txtMiss
+            // 
+            this.txtMiss.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtMiss.Location = new System.Drawing.Point(112, 36);
+            this.txtMiss.MaxLength = 5;
+            this.txtMiss.Name = "txtMiss";
+            this.txtMiss.Size = new System.Drawing.Size(100, 19);
+            this.txtMiss.TabIndex = 1;
+            this.txtMiss.Text = "2";
+            this.txtMiss.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtMiss.WordWrap = false;
+            this.txtMiss.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // txtMissRemove
+            // 
+            this.txtMissRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtMissRemove.Location = new System.Drawing.Point(114, 40);
+            this.txtMissRemove.MaxLength = 5;
+            this.txtMissRemove.Name = "txtMissRemove";
+            this.txtMissRemove.Size = new System.Drawing.Size(100, 19);
+            this.txtMissRemove.TabIndex = 3;
+            this.txtMissRemove.Text = "1";
+            this.txtMissRemove.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtMissRemove.WordWrap = false;
+            this.txtMissRemove.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // txtHitRemove
+            // 
+            this.txtHitRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtHitRemove.Location = new System.Drawing.Point(8, 40);
+            this.txtHitRemove.MaxLength = 5;
+            this.txtHitRemove.Name = "txtHitRemove";
+            this.txtHitRemove.Size = new System.Drawing.Size(100, 19);
+            this.txtHitRemove.TabIndex = 2;
+            this.txtHitRemove.Text = "0";
+            this.txtHitRemove.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtHitRemove.WordWrap = false;
+            this.txtHitRemove.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // txtRunCnt
+            // 
+            this.txtRunCnt.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtRunCnt.Location = new System.Drawing.Point(12, 184);
+            this.txtRunCnt.MaxLength = 9;
+            this.txtRunCnt.Name = "txtRunCnt";
+            this.txtRunCnt.Size = new System.Drawing.Size(100, 19);
+            this.txtRunCnt.TabIndex = 4;
+            this.txtRunCnt.Text = "100";
+            this.txtRunCnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtRunCnt.WordWrap = false;
+            this.txtRunCnt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(126, 184);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(105, 19);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "実行";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 209);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(221, 19);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "初期化";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "あたりの数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "はずれの数";
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.txtHit);
+            this.gb1.Controls.Add(this.txtMiss);
+            this.gb1.Controls.Add(this.label2);
+            this.gb1.Controls.Add(this.label1);
+            this.gb1.Location = new System.Drawing.Point(12, 12);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(221, 69);
+            this.gb1.TabIndex = 9;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "あたり／はずれの数";
+            // 
+            // gb2
+            // 
+            this.gb2.Controls.Add(this.label3);
+            this.gb2.Controls.Add(this.label4);
+            this.gb2.Controls.Add(this.txtHitRemove);
+            this.gb2.Controls.Add(this.txtMissRemove);
+            this.gb2.Location = new System.Drawing.Point(12, 87);
+            this.gb2.Name = "gb2";
+            this.gb2.Size = new System.Drawing.Size(221, 77);
+            this.gb2.TabIndex = 10;
+            this.gb2.TabStop = false;
+            this.gb2.Text = "選択後に取り除く値";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "はずれの数";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "あたりの数";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "実行回数";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(6, 18);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(261, 15);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 14;
+            // 
+            // gb3
+            // 
+            this.gb3.Controls.Add(this.progressBar);
+            this.gb3.Location = new System.Drawing.Point(243, 12);
+            this.gb3.Name = "gb3";
+            this.gb3.Size = new System.Drawing.Size(273, 46);
+            this.gb3.TabIndex = 15;
+            this.gb3.TabStop = false;
+            this.gb3.Text = "処理進捗";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(243, 65);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(273, 166);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(247, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "結果表示";
+            // 
+            // chcSelect
+            // 
+            this.chcSelect.AutoSize = true;
+            this.chcSelect.Location = new System.Drawing.Point(128, 166);
+            this.chcSelect.Name = "chcSelect";
+            this.chcSelect.Size = new System.Drawing.Size(87, 16);
+            this.chcSelect.TabIndex = 17;
+            this.chcSelect.Text = "選択を変える";
+            this.chcSelect.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 242);
+            this.Controls.Add(this.chcSelect);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.gb3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gb2);
+            this.Controls.Add(this.gb1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtRunCnt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monty Hall problem";
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.gb2.ResumeLayout(false);
+            this.gb2.PerformLayout();
+            this.gb3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHit;
+        private System.Windows.Forms.TextBox txtMiss;
+        private System.Windows.Forms.TextBox txtMissRemove;
+        private System.Windows.Forms.TextBox txtHitRemove;
+        private System.Windows.Forms.TextBox txtRunCnt;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.GroupBox gb2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.GroupBox gb3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chcSelect;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+
+namespace Monty_Hall_problem
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private int TotalLottery;
+        private int TotalHitCnt;
+        private int RemoveHitCnt;
+        private int RemoveMissCnt;
+        private int RunCnt;
+
+        static System.Random r = new System.Random();
+
+        private void TextBox_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e) 
+        {
+            if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b') e.Handled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtinitialize();
+        }
+
+        private void txtinitialize()
+        {
+            txtHit.Text = "1";
+            txtMiss.Text = "2";
+            txtHitRemove.Text = "0";
+            txtMissRemove.Text = "1";
+            txtRunCnt.Text = "100";
+        }
+        
+        private void GetPlayData()
+        {
+            TotalHitCnt = int.Parse(txtHit.Text);
+            TotalLottery = TotalHitCnt + int.Parse(txtMiss.Text);
+            RemoveHitCnt = int.Parse(txtHitRemove.Text);
+            RemoveMissCnt = int.Parse(txtMissRemove.Text);
+            RunCnt = int.Parse(txtRunCnt.Text);
+        }
+
+        public static void SetProgressBarValue(ProgressBar pb, int val)
+        {
+            if (pb.Value < val)
+            {
+                //値を増やす時
+                if (val < pb.Maximum)
+                {
+                    //目的の値より一つ大きくしてから、目的の値にする
+                    pb.Value = val + 1;
+                    pb.Value = val;
+                }
+                else
+                {
+                    //最大値にする時
+                    //最大値を1つ増やしてから、元に戻す
+                    pb.Maximum++;
+                    pb.Value = val + 1;
+                    pb.Value = val;
+                    pb.Maximum--;
+                }
+            }
+            else
+            {
+                //値を減らす時は、そのまま
+                pb.Value = val;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+            int u;
+            int Hit = 0;
+            int Miss = 0;
+            int progress = 0;
+            int hisprogress = 0;
+
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            btnRun.Text = "実行中";
+            btnRun.Enabled = false;
+
+            gb3.Text = "処理進捗";
+
+            SetProgressBarValue(progressBar, progress);
+
+            //画面上の値を読み取る
+            GetPlayData();
+            
+            //処理
+            for (u = 0; u < RunCnt; u++)
+            {
+                if (Monty()) Hit++;
+                else Miss++;
+
+                //プログレスバーの更新(めもりは100)
+                progress = (int)((u + 1f) / RunCnt * 100f);
+                if (progress != hisprogress)
+                {
+                    SetProgressBarValue(progressBar, progress);
+                    hisprogress = progress;
+                }
+            }
+
+            if ( RunCnt != 1)
+                CreateChart(Hit, Miss);
+
+            btnRun.Text = "実行";
+            btnRun.Enabled = true;
+
+            sw.Stop();
+            gb3.Text = $"処理時間：{sw.Elapsed.Hours}時間 {sw.Elapsed.Minutes}分 {sw.Elapsed.Seconds}秒 {sw.Elapsed.Milliseconds}ミリ秒";
+        }
+
+        private void CreateChart(int Hit, int Miss)
+        {
+            // フォームをロードするときの処理
+            chart1.Series.Clear();  // ← 最初からSeriesが1つあるのでクリアします
+            chart1.ChartAreas.Clear();
+
+            // ChartにChartAreaを追加します
+            string chart_area1 = "Area1";
+            chart1.ChartAreas.Add(new ChartArea(chart_area1));
+            // ChartにSeriesを追加します
+            string legend1 = "Graph1";
+            chart1.Series.Add(legend1);
+            // グラフの種別を指定
+            chart1.Series[legend1].ChartType = SeriesChartType.Pie; // 円グラフを指定してみます
+
+            double[] values = new double[2] { Hit , Miss };
+
+            // 各項目の値を加算して合計(全体の大きさ)を算出します
+            double total = 0.0;
+            foreach (double d in values)
+            {
+                total += d;
+            }
+            // データをシリーズにセットします
+            for (int i = 0; i < values.Length; i++)
+            {
+                double rate = (values[i] / total) * 100.0;  // <-- ここで割合を算出します
+                DataPoint dp = new DataPoint(rate, rate);
+                dp.AxisLabel = rate.ToString() + "%";
+                chart1.Series[legend1].Points.Add(dp);
+            }
+            chart1.Series[legend1].Points[0].AxisLabel = "あたり\n" + chart1.Series[legend1].Points[0].AxisLabel;
+            chart1.Series[legend1].Points[1].AxisLabel = "はずれ\n" + chart1.Series[legend1].Points[1].AxisLabel;
+        }
+
+        private bool Monty()
+        {
+            int i, n, select;
+            bool[] data = new bool[0];
+            bool[] removedata = new bool[0];
+
+            //あたり + はずれの分、配列を用意する
+            Array.Resize(ref data, TotalLottery);
+            Array.Resize(ref removedata, TotalLottery);
+
+            //あたりの設定
+            i = 0;
+            while(i < TotalHitCnt)
+            {
+                n = r.Next(0, TotalLottery);
+
+                if (!data[n]) data[n] = true;
+                else continue;
+                i++;
+            }
+
+            //プレイヤーの選択
+            select = r.Next(0, TotalLottery);
+
+            //選択以外のあたり／はずれを取り除く数分除去
+            i = 0;
+            //あたりを除去する
+            while (i < RemoveHitCnt)
+            {
+                //乱数取得
+                n = r.Next(0, TotalLottery);
+
+                //プレイヤーが選択したものであれば飛ばす
+                if (n == select) continue;
+
+                //あたりかつまだ除去されていない場合除去
+                if (data[n] && !removedata[n])
+                {
+                    removedata[n] = true;
+                    i++;
+                }
+            }
+
+            //はずれを除去する
+            i = 0;
+            while (i < RemoveMissCnt)
+            {
+                //乱数取得
+                n = r.Next(0, TotalLottery);
+
+                //プレイヤーが選択したものであれば飛ばす
+                if (n == select) continue;
+
+                //あたりかつまだ除去されていない場合除去
+                if (!data[n] && !removedata[n])
+                {
+                    removedata[n] = true;
+                    i++;
+                }
+            }
+
+            if(chcSelect.Checked)
+            {
+                //選択を変える場合
+                while(true)
+                {
+                    //乱数取得
+                    n = r.Next(0, TotalLottery);
+                    //すでに選択している場合、飛ばす
+                    if (n == select) continue;
+                    //除去済の場合、飛ばす
+                    if (removedata[n]) continue;
+                    //プレイヤーの選択(変更後)
+                    select = n;
+                    break;
+                }
+            }
+            //結果を返す
+            return data[select];
+        }
+    }
+}
